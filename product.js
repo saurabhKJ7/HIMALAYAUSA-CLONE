2fcda84d7abc21e6a4a2a440b785c1b0af6c81b1
sonukalakar <ajaypandat329@gmail.com>
2022-05-09 19:18:38 +0530

    part3

---
+++ product.js
@@ -0,0 +1,198 @@
+var productdata=[
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/image-3_1024x.png?v=1591028195",
+      name: "Back to Balance Cleanse™",
+      position: "$29.95",
+      product_id:1,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/HimalayaBambooToothpaste-Tube_Front_1024x.png?v=1613065864",
+      name: "Bamboo & Sea Salt Whitening Antiplaque Toothpaste",
+      position: "$5.99",
+      product_id:2,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/BEAUTY_bodybar_purifying-2_1024x.png?v=1595909766",
+      name: "Body Bar: Purifying (Neem & Turmeric)",
+      position: "$4.99",
+      product_id:3,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/BEAUTY_bodybar_refreshing-1_1024x.png?v=1595909931",
+      name: "Body Bar: Refreshing (Lavender & Rosemary)",
+      position: "$4.99",
+      product_id:4,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/Boswellia-60---Box-Bottle_1024x.png?v=1595862802",
+      name: "Boswellia",
+      position: "$14.95",
+      product_id:5,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/HimalayaCharcoalToothpaste-Tube_Front_1024x.png?v=1613059882",
+      name: "Charcoal & Black Seed Oil Whitening Antiplaque Toothpaste",
+      position: "$5.99",
+      product_id:6,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/HimalayaChestBalmPMwithHerbCartonJar_1024x.png?v=1624548043",
+      name: "Chest Balm P.M.",
+      position: "$7.99",
+      product_id:7,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/image-58_1024x.png?v=1591027935",
+      name: "Chyavanprash",
+      position: "$13.95",
+      product_id:8,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/image-33_1024x.png?v=1591027962",
+      name: "ComfortCleanse®",
+      position: "$20.95",
+      product_id:9,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/image-30_1024x.png?v=1590836814",
+      name: "Curcumin Complete™",
+      position: "$18.99",
+      product_id:10,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/image-34_1024x.png?v=1591027900",
+      name: "DermaCare®",
+      position: "$10.99",
+      product_id:11,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+    {
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0399/1728/9633/products/BEAUTY_facewash_exoliatingwwa_bottlebox_1024x.png?v=1595909354",
+      name: "Face Scrub: Exfoliating (Walnut & Wood Apple)",
+      position: "$8.99",
+      product_id:12,
+      logo: "https://lh3.googleusercontent.com/gE-Epy_M7QINR02p_AaErAcYz2yJQSIwVlrkZhwltHTR2LUwE6C5DvQIl7C92FjK3-_VLp1LkZ9nRZgc39dtUei6wuXICpJNhlXTa8hWdadTGTGOkL66x6pnOxadNXm_25n03i_GJA=s55-p-k",
+    },
+   
+  ];
+      
+// var str=productdata.map(function(elem){
+//     return +(elem.position.replace("$",""))
+// })
+// console.log(str)
+// var arr1=localStorage.setItem("product",JSON.stringify("productdata"))
+// var arr=JSON.parse(localStorage.getItem("product"))
+  
+   displayproduct(productdata)
+
+   var cartarr=JSON.parse(localStorage.getItem("cartdata"))||[];
+   console.log(cartarr)
+
+   function displayproduct(arr){
+       document.getElementById("productcategory").innerHTML="";
+       arr.map(function(elem){
+           var box=document.createElement("div")
+            var pimg= document.createElement("img");
+            pimg.setAttribute("src", elem.avatar);
+           
+           var pnames=document.createElement("p")
+           pnames.innerText=elem.name
+
+           var pprice=document.createElement("p")
+           pprice.innerText=elem.position
+
+           var plogo=document.createElement("img")
+           plogo.setAttribute("src",elem.logo)
+
+           var btn=document.createElement("button")
+            var p=document.createElement("p")
+            p="Add to cart"
+            btn.addEventListener("click",function(){
+              carddatafunction(elem)
+            });
+            btn.append(plogo,p)
+
+           box.append(pimg,pnames,pprice,btn)
+
+           document.querySelector("#productcategory").append(box)
+
+       })
+   }
+  
+   function carddatafunction(elem){
+      var a=cartarr.filter(function(el){
+       return el.product_id == elem.product_id;
+      })
+ 
+      if(a.length === 0){
+        cartarr.push(elem);
+        alert("Product Added to Cart Successfuly")
+        localStorage.setItem("cartdata",JSON.stringify(cartarr))
+      }else{
+        alert("Product already in Cart");
+      }
+     }
+   
+
+
+ function filtervalue() {
+      var selected = document.querySelector("#filter").value;
+      
+   if (selected == "pasc") {
+        productdata.sort(function (a, b) {
+          return +(a.position.replace("$","")) - (+(b.position.replace("$","")) );;
+        });
+        displayproduct(productdata);
+      }
+
+      if (selected == "pdsc") {
+        productdata.sort(function (a, b) {
+          return +(b.position.replace("$","")) - (+(a.position.replace("$","")) );
+        });
+        displayproduct(productdata);
+      }
+
+      if (selected == "nasc") {
+        productdata.sort(function (a, b) {
+          if (a.name > b.name) return 1;
+          if (a.name < b.name) return -1;
+          return 0;
+        });
+         displayproduct(productdata)
+         
+        
+      }
+      if (selected == "ndsc") {
+        productdata.sort(function (a, b) {
+          if (a.name > b.name) return -1;
+          if (a.name < b.name) return 1;
+          return 0;
+        });
+        displayproduct(productdata);
+        
+        
+      }
+    }
+     
